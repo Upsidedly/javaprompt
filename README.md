df8fd528cf4830eab18842bb6c8451aa19d0681b
Upsided <matthewlloydw@gmail.com>
2022-03-20 18:49:29 +0000

    Replaced docstring with README.

---
+++ README.md
@@ -0,0 +1,21 @@
+# javaprompt
+The javaprompt package which includes several dynamic input functions created by upsided.
+
+## Installation
+```bash
+npm i javaprompt
+yarn add javaprompt
+pnpm add javaprompt
+```
+---
+
+
+## History
+### 1.0.0
+- Added `input()` command.
+    - Supports return types of `string`, `number`, and `integer`, any other type selected will show the prompt but return an empty string
+- Simple `rl.ts` file which has the readline instance and configuration.
+
+## Functions
+
+### `input()`
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-/*
-The main file for javaprompt package created by upsided. Will have several input functions including input, confirm, checkbox, etc.
-
-CHANGELOG:
-input() 1.0.0 (Incomplete)
-*/
-
 import { rl } from "./rl.js"
 import { promisify } from 'util';
 import chalk from 'chalk';
@@ -12,15 +5,28 @@ import chalk from 'chalk';
 type StringTypes = 'string' | 'boolean' | 'integer' | 'float' | 'array' | 'number'
 type InputTypes = string | boolean | number | any[]
 type InputOptions = {
-    /** Input return type, which includes `string` (1.0.0), `number` (1.0.0), `integer` (1.0.0)
+    /**
+     * Input return type, which includes `string` (1.0.0), `number` (1.0.0), `integer` (1.0.0)
     */
     type: StringTypes
 }
 
 /**
- * @since 1.0.0
+ * Creates an input prompt in the console which can return several types depending on which specified
  * @param message The message to print when requesting input.
- * @param options Input options @see InputOptions
+ * @param options {@link InputOptions} Input options
+ * @example ```js
+ * import { input } from 'javaprompt';
+ * 
+ * const response = await input('Give a number! ', { type: number }); 
+ * ```
+ * Console:
+ * ```node
+ * $ node . 
+ * ? Give a number!
+ * ```
+ * Input typing follows the message.
+ * @since 1.0.0
  * @returns {Promise<InputTypes>} One of the types in StringTypes. Some have been yet to be implemented
  */
 export async function input(message: string, options?: InputOptions): Promise<InputTypes> {
@@ -39,18 +45,27 @@ export async function input(message: string, options?: InputOptions): Promise<In
     if (!options) {
         // If no type is provided, the default is string
         return response
+
     } else if (options.type === 'string') {
+        // Same as default
         return response
+
     } else if (options.type === 'integer') {
-        if (Number.isInteger(parseFloat(response)) && !/[^1-9]/.test(response)) {
+
+        // Checks if the string only contains numerical characters (0-9), meaning its an integer.
+        if (!/[^1-9]/.test(response)) {
             return Number.parseInt(response)
-        } else {
+        } else { // Isn't integer
+
+            // Keeps prompting until input finally is an integer
             while (true) {
                 console.log(`${chalk.green('!')} Invalid input. please provide an integer.`)
                 process.stdout.write(`${chalk.green('?')} ${message}`)
                 rl.resume()
                 const res = await question(message)
                 rl.pause()
+
+                // If it's finally an integer
                 if (Number.isInteger(parseFloat(res)) && !/[^1-9]/.test(res)) {
                     return Number.parseInt(res)
                 }
